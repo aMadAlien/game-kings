2683ff3917576f93687c86a5f918917f03d4cfec
iren <irasibil2@gmail.com>
2023-12-26 09:58:24 +0200

    create player

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16
 canvas.height = 64 * 8
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+const player = new Player()
+
+// let bottom = y + 100
+
+function animate() {
+    window.requestAnimationFrame(animate)
+
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.draw()
+    player.update()
+}
+
+animate()
