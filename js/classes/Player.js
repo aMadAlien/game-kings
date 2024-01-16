15f7a9faaf3d4490e0ced6a30b49a037a885d98a
iren <irasibil2@gmail.com>
2024-01-16 12:23:50 +0200

    add animations for movements

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,9 +1,9 @@
 class Player extends Sprite {
     constructor({
         collisionBlocks = [],
-        imageSrc, frameRate
+        imageSrc, frameRate, animations
     }) {
-        super({ imageSrc, frameRate })
+        super({ imageSrc, frameRate, animations })
 
         this.position = {
             x: 200,
@@ -38,6 +38,13 @@ class Player extends Sprite {
         this.updateHitbox()
 
         this.checkForVerticalCollisions()
+    }
+    switchSprite(name) {
+        if (this.image === this.animations[name].image) return
+        this.currentFrame = 0
+        this.image = this.animations[name].image
+        this.frameRate = this.animations[name].frameRate
+        this.frameBuffer = this.animations[name].frameBuffer
     }
     updateHitbox() {
         this.hitbox = {
