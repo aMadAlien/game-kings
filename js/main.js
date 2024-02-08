c080edc5cbee07d2d5b2a6c01389171a374dc611
iren <irasibil2@gmail.com>
2024-02-08 12:02:33 +0200

    create levels map window

--- js/main.js
+++ js/main.js
@@ -3,8 +3,19 @@ const welcomeWindow = document.getElementById('welcome-window')
 
 if (!welcomeWindow.classList.contains('move-left')) {
     openMapBtn.addEventListener('click', () => {
-        console.log('add');
         welcomeWindow.classList.add('move-left')
     })
 }
 
+const mapWindow = document.getElementById('map-window')
+const levelsContainer = document.getElementsByClassName('map-container')[0]
+
+if (!mapWindow.classList.contains('move-left')) {
+    for (let i = 0; i < Object.keys(levels).length; i++) {
+        const newLevelBtn = document.createElement('button')
+        newLevelBtn.setAttribute('role', 'button')
+        newLevelBtn.setAttribute('class', 'level-btn btn')
+        newLevelBtn.innerText = i + 1
+        levelsContainer.appendChild(newLevelBtn)
+    }
+}
