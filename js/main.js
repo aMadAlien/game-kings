7bcafbd7e4ce42d7c7d7fcead83d64cb663bf174
iren <irasibil2@gmail.com>
2024-02-08 13:12:50 +0200

    style levels map

--- js/main.js
+++ js/main.js
@@ -10,12 +10,28 @@ if (!welcomeWindow.classList.contains('move-left')) {
 const mapWindow = document.getElementById('map-window')
 const levelsContainer = document.getElementsByClassName('map-container')[0]
 
+localStorage.setItem('game-data', JSON.stringify({ lastLevel: 2 }))
+
 if (!mapWindow.classList.contains('move-left')) {
-    for (let i = 0; i < Object.keys(levels).length; i++) {
+    for (let i = 1; i <= Object.keys(levels).length; i++) {
         const newLevelBtn = document.createElement('button')
         newLevelBtn.setAttribute('role', 'button')
         newLevelBtn.setAttribute('class', 'level-btn btn')
-        newLevelBtn.innerText = i + 1
+        newLevelBtn.innerText = i
+
+        const userData = JSON.parse(localStorage.getItem('game-data'))
+
+        if (userData) {
+            const lastLevel = userData.lastLevel
+            if (lastLevel > i) {
+                newLevelBtn.classList.add('level-done')
+            } else if (lastLevel === i) {
+                newLevelBtn.classList.add('level-current')
+            } else {
+                newLevelBtn.classList.add('level-blocked')
+            }
+        }
+
         levelsContainer.appendChild(newLevelBtn)
     }
 }
